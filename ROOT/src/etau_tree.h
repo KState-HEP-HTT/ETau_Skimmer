3be8b706b6872101dab50bd6a13512b8d4714eeb
Tyler Mitchell <tyler106@ksu.edu>
2018-09-12 11:01:38 -0500

    apply the lepton discriminators in skimmer instead

--- ROOT/src/etau_tree.h
+++ ROOT/src/etau_tree.h
@@ -231,6 +231,9 @@ void etau_tree::do_skimming() {
     if ((!tByVLooseIsolationMVArun2v1DBoldDMwLT && !tRerunMVArun2v1DBoldDMwLTVLoose) || !tDecayModeFinding || fabs(tCharge) > 1) // tau quality selection
       continue;
 
+    if (!tAgainstMuonLoose3 || !tAgainstElectronTightMVA6) // tau against leptons
+      continue;
+
     if (muVetoZTTp001dxyzR0 > 0 || eVetoZTTp001dxyzR0 > 1 || dielectronVeto > 0) // vetos
       continue;
 
